c2b6c519868316035236d3561c8baedd191a4de2
Seukwon Kang <kasworld@gmail.com>
2021-04-06 15:24:02 +0900

    del wasm build

--- rundriver/genwasmclient.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-BUILD_VER=${1}
-
-echo "GOOS=js GOARCH=wasm go build -o clientdata/wasmclientgl.wasm -ldflags "-X main.Ver=${BUILD_VER}" wasmclientgl.go"
-GOOS=js GOARCH=wasm go build -o clientdata/wasmclientgl.wasm -ldflags "-X main.Ver=${BUILD_VER}" wasmclientgl.go
